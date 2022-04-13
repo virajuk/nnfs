d3a12be46524530e32848638e0d757d5756f7122
virajuk <virajudayakumara@gmail.com>
2022-04-13 10:53:14 -0400

    temp

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+inputs = np.array([1, 2, 3, 2.5])
+weights = np.array([[0.2, 0.8, -0.5, 1],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]])
+biases = np.array([2])
+
+outputs = np.dot(weights, inputs) + biases
+print(type(outputs))
+print(outputs)
