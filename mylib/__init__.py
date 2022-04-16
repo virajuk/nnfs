f1fda770d3d80ed09e5970062bbe629fa1f30b8a
virajuk <virajudayakumara@gmail.com>
2022-04-16 02:32:30 -0400

    __init__

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ['LayerDense', 'ActivationReLU']
+__all__ = ['LayerDense', 'ActivationReLU', 'ActivationSoftmax']
 from .layer_dense import *
 from .activation_relu import *
+from .activation_softmax import *
