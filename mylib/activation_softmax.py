971dd4e641ca6e707a033bccda75a5e833842200
virajuk <virajudayakumara@gmail.com>
2022-04-20 11:28:53 -0400

    doc

--- mylib/activation_softmax.py
+++ mylib/activation_softmax.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 
+# Softmax activation
 class ActivationSoftmax:
 
+    # forward pass
     def forward(self, inputs):
-
+        # un-normalized probabilities
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-
+        # normalize them for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
         self.output = probabilities
