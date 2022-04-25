c7ff9b8fc6fb7cfa2388f673854dc261a5aebbf0
virajuk <virajudayakumara@gmail.com>
2022-04-25 12:13:27 -0400

    numpy axis test

--- third.py
+++ third.py
@@ -1,3 +1,19 @@
+import math
 import numpy as np
 
 import nnfs
+
+# layer_outputs = [4.8, 1.21, 2.385]
+
+E = math.e
+
+# print(E)
+
+layer_outputs = np.array([[4.8, 1.21, 2.385],
+                          [8.9, -1.81, 0.2],
+                          [1.41, 1.051, 0.026]])
+
+result = np.sum(layer_outputs, axis=0, keepdims=True)
+
+print(result.shape)
+print(result)
