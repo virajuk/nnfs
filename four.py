f576f3674e43221c8271ed1ff89a43fef2e1e0ef
virajuk <virajudayakumara@gmail.com>
2022-05-02 12:48:31 -0400

    string literals

--- four.py
+++ four.py
@@ -16,14 +16,15 @@ loss = -(math.log(softmax_output[0])*target_output[0] +
 
 softmax_outputs = np.array([[0.7, 0.1, 0.2],
                             [0.1, 0.5, 0.4],
-                            [0.02, 0.9, 0.08]])
+                            [0.02, 0.9, 0.08],
+                            [0.2, 0.09, 0.71]])
 
-class_targets = np.array([0, 1, 1])
+class_targets = np.array([0, 1, 1, 2])
 
-# print(class_targets.shape)
+rows, cols = softmax_outputs.shape
 # print(np.sum(class_targets, axis=0))
 
 # for target_idx, distribution in zip(class_targets, softmax_outputs):
 #     print(distribution[target_idx])
 
-print(softmax_outputs[[0, 1, 2], class_targets])
+print(softmax_outputs[[x for x in range(0, rows)], class_targets])
