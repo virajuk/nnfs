9a20e643e11d48f33144cc5be912ea819897c2c5
virajk <virajk@saksglobal.com>
2022-05-03 02:45:05 -0400

    reverse

--- four.py
+++ four.py
@@ -11,6 +11,9 @@ loss = -(math.log(softmax_output[0])*target_output[0] +
          math.log(softmax_output[1])*target_output[1] +
          math.log(softmax_output[2])*target_output[2])
 
+# print(loss)
+# print(math.log10(100))
+
 softmax_outputs = np.array([[0.7, 0.1, 0.2],
                             [0.1, 0.5, 0.4],
                             [0.02, 0.9, 0.08],
