f95b9508de099d5267d0a90edb3797cc044af1f1
virajuk <virajudayakumara@gmail.com>
2022-05-03 02:55:13 -0400

    test git local configs

--- four.py
+++ four.py
@@ -11,6 +11,8 @@ loss = -(math.log(softmax_output[0])*target_output[0] +
          math.log(softmax_output[1])*target_output[1] +
          math.log(softmax_output[2])*target_output[2])
 
+
+
 print(loss)
 print(math.log10(100))
 
