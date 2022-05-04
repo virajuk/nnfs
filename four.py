1794724cce8971c82f36dd0d6684fb167d296ee3
virajuk <virajudayakumara@gmail.com>
2022-05-04 11:31:33 -0400

    loss func for sparse and one hot encoded vectors

--- four.py
+++ four.py
@@ -2,35 +2,26 @@ import math
 
 import numpy as np
 
-softmax_output = [0.7, 0.1, 0.2]
-
-
-target_output = [1, 0, 0]
-
-# loss = -(math.log(softmax_output[0])*target_output[0] +
-#          math.log(softmax_output[1])*target_output[1] +
-#          math.log(softmax_output[2])*target_output[2])
-
-# print(loss)
-# print(math.log10(100))
-
 softmax_outputs = np.array([[0.7, 0.1, 0.2],
                             [0.1, 0.5, 0.4],
                             [0.02, 0.9, 0.08],
                             [0.2, 0.09, 0.71]])
 
-class_targets = np.array([0, 1, 1, 2])
+class_targets = np.array([[1, 0, 0],
+                          [0, 1, 0],
+                          [0, 1, 0],
+                          [0, 0, 1]])
 
-rows, cols = softmax_outputs.shape
-# print(np.sum(class_targets, axis=0))
+# class_targets = np.array([0, 1, 1, 2])
 
-# for target_idx, distribution in zip(class_targets, softmax_outputs):
-#     print(distribution[target_idx])
+if len(class_targets.shape) == 1:
+    correct_confidences = softmax_outputs[range(len(softmax_outputs)), class_targets]
+elif len(class_targets.shape) == 2:
+    correct_confidences = np.sum(softmax_outputs*class_targets, axis=1)
 
-# print(softmax_outputs[[x for x in range(0, rows)], class_targets])
-print(softmax_outputs[range(len(softmax_outputs)), class_targets])
+print(correct_confidences)
 
-neg_log = -np.log(softmax_outputs[range(len(softmax_outputs)), class_targets])
+neg_log = -np.log(correct_confidences)
 print(neg_log)
 
 average_loss = np.mean(neg_log)
