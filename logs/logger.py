2b788675a5059e992abd13ab56b805efa1dc90ba
virajuk <virajudayakumara@gmail.com>
2022-05-17 03:17:27 -0400

    minor updates

--- logs/logger.py
+++ logs/logger.py
@@ -5,18 +5,22 @@ LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logname = "logs/my_app.log"
 
 
-def get_logger():
+def get_logger(logger_name):
 
-    logger = logging.getLogger('my_app')
+    if logging.getLogger(logger_name).hasHandlers():
+        return logging.getLogger(logger_name)
+
+    logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
     logger = time_handler(logger)
+    logger.propagate = False
     return logger
 
 
 def time_handler(logger):
 
-    handler = TimedRotatingFileHandler(filename=logname, when="midnight", interval=1)
+    handler = TimedRotatingFileHandler(filename=logname, when="midnight", interval=1, backupCount=30)
     handler.suffix = "%Y-%m-%d"
     formatter = logging.Formatter(LOG_FORMAT)
 
